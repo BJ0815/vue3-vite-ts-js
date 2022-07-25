cdb28343b711ac3382851ecf549fe96c6cafdb77
BJ0815 <jtt11220@gmail.com>
2022-07-25 21:46:06 +0800

    Init

---
+++ src/stores/counter.ts
@@ -0,0 +1,16 @@
+import { defineStore } from 'pinia'
+
+export const useCounterStore = defineStore({
+  id: 'counter',
+  state: () => ({
+    counter: 0
+  }),
+  getters: {
+    doubleCount: (state): number => state.counter * 2
+  },
+  actions: {
+    increment() {
+      this.counter++
+    }
+  }
+})
